b64e3bddbb83d8f0be1bbe27d532e5b1be287c92
antraxsec <39973938+antraxsec@users.noreply.github.com>
2023-04-16 11:09:31 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,97 +12,79 @@ import requestCounterMiddleware from "./requestCounterMiddleware.js";
 import { PORT } from "./config.js";
 
 const app = express();
-const server = createServer(app); // Crear un servidor HTTP para Express y Socket.IO
-const io = new Server(server, { cors: { origin: "*" } }); // Configurar Socket.IO
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(cors());
 app.use(express.json());
-//incorporar para ver cuantos consumen mi servicio
-app.use(requestCounterMiddleware(io)); // Agrega el middleware para ver cuantos consultan la api en tiempo real
+app.use(requestCounterMiddleware(io));
 app.use(indexRouter);
 app.use("/api", items);
-app.post("/webhook", (req, res) => {
-  const data = req.body;
-  console.log(data);
-  res.sendStatus(200); // Responde al webhook con un estado HTTP 200 OK
-});
 
-// Contador de usuarios activos
-let activeUsers = 0;
-let datosDavid = [];
-let objeto = [];
-const users = new Map();
-// Manejar conexiones y desconexiones de Socket.IO
+let dataTracking = [];
+let dataUsuarios = [];
+let usuariosActivos = 0;
+const usuarios = new Map();
+
 io.on("connection", (socket) => {
-  activeUsers++;
-  /////////////////////////////////////////////////
-  socket.on("user connected", (username) => {
-    socket.username = username;
-    console.log(username);
-    users.set(socket.id, { id: socket.id, name: username }); // Almacenar la información del usuario con su socket.id
-    io.emit("update users", Array.from(users.values()));
-  });
+	
+	usuariosActivos++;
+	console.log("que tal metal");
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
+	io.emit("idUsuario", socket.id);
+	io.emit("usuariosActivos", usuariosActivos);
 
-  socket.on("private message", (msg) => {
-    const recipientSocket = io.sockets.sockets.get(msg.recipient);
-    if (recipientSocket) {
-      // Enviar mensaje al destinatario
-      recipientSocket.emit("private message", {
-        user: msg.user,
-        text: msg.text,
-      });
-      // Enviar mensaje de confirmación al emisor
-      socket.emit("private message", {
-        fromSelf: true,
-        recipientName: recipientSocket.username,
-        text: msg.text,
-      });
-    }
-  });
-  //////////////////////////////////////////////
-  //saver cuantos usuarios estan conectados
-  io.emit("activeUsers", activeUsers);
-  //enviar id de usuario
-  io.emit("idUsuario", socket.id);
-  //resivir datos de david
-  socket.on("datosDavid", (data) => {
-    //console.log(data);
-    data.id = socket.id;
-    datosDavid.push(data);
+	let usuario = {};
+		usuario.id = socket.id;
+		dataUsuarios.push(usuario);
 
-    //console.log("enviando datos de david");
-    io.emit("todosDatosDavid", datosDavid);
-  });
-  socket.on("tracking", (data) => {
-    objeto.push(data);
+	// socket.on("user connected", (username) => {
+	// 	socket.username = username;
+	// 	console.log('Este es nombre de usuairo:: ', username);
+	// 	usuarios.set(socket.id, { id: socket.id, name: username });
+	// 	io.emit("update users", Array.from(usuarios.values()));
+	// });
 
-    //console.log("enviando datos de david");
-    io.emit("dataTracking", objeto);
-  });
+	socket.on("userTracer", (data) => {
+		console.log(data);
 
-  socket.on("disconnect", () => {
-    activeUsers--;
-    io.emit("activeUsers", activeUsers);
-    //eliminar usuario de la lista  de usuarios conectados
-    //console.log(datosDavid);
-    datosDavid = datosDavid.filter((item) => item.id !== socket.id);
-    //console.log(datosDavid);
+		const dispositivo = data.dispositivo_cookie;
+		const pais 	      = data.pais_trackingsesion;
+		const idsesion    = data.id_sesion;
 
-    //console.log("usuario desconectado");
-    io.emit("todosDatosDavid", datosDavid);
+		const existeSesion = dataTracking.some((item) => item.id_sesion === idsesion);
+		// if (!existeSesion) {
+			dataTracking.push(data);
+		// }
+		// else{
+		// 	console.log('Esta sesion ya existe, continua');
+		// }
 
-    users.delete(socket.id);
-    io.emit("update users", Array.from(users.values()));
-  });
+		if (pais !== null) {
+			console.log("El país es:: ".pais);
+		} else {
+			console.log("No llego datos de geolocalización");
+		}
+
+		io.emit("serverTracer", dataTracking, dataUsuarios);
+	});
+
+	socket.on("disconnect", () => {
+		usuariosActivos--;
+		io.emit("usuariosActivos", usuariosActivos);
+		// //eliminar usuario de la lista  de usuarios conectados
+		dataUsuarios = dataUsuarios.filter((item) => item.id !== socket.id);
+		console.log(dataUsuarios);
+
+		usuarios.delete(socket.id);
+		io.emit("update users", Array.from(usuarios.values()));
+	  });
 });
 
 app.use(express.static("public"));
 
 export default server;
+
 
 /// donde esta el servidor
 ///https://railway.app/project/2a24385c-f6d0-46e2-a79e-ce4460940421/service/4719a159-1bd3-433e-87bd-d5b5bd538b91?id=e38a0adf-18f8-45a7-a0f9-c9a75d0cf90d
