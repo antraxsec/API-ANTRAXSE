c494d5967bd3879683b73a59af4fa1ab13f24c3f
antraxsec <39973938+antraxsec@users.noreply.github.com>
2023-08-16 14:33:40 -0400

    sunegue

--- src/app.js
+++ src/app.js
@@ -64,12 +64,7 @@ app.post("/webhookwhatsapp", function (request, response) {
 				mensajeFacebook(numero, `¡Hola! Puedes encontrar todos los detalles y realizar la compra de la Samsung Galaxy Book en nuestra tienda 👉 multi.bz/samsung 🛒💻.`);
 				mensajeFacebook(numero, `Si realizas tu compra, pedido o reserva hoy, ${dia}, puedes retirarlo en nuestra tienda física desde las 11:00 a 19:00 o solicitar el envío a donde estés.`);
 				mensajeFacebook(numero, `Te dejo algunos modelos de nuestros productos más demandados. Si deseas ver los precios actualizados, simplemente haz clic en el enlace proporcionado.`);
-				productoFacebook(numero, "100354", "100279", `*Código SKU:* 100279
-*Procesador:* Intel Core i3 a 4,1Ghz de 11a. Gen.
-*Memoria RAM:* 8GB a 2666 Mhz
-*Almacenamiento:* SSD NVME 256 GB
-*Pantalla:* 15,6" LED FULLHD (1920 x 1080)`
-					`*(Bs. 3300) Ver precio actualizado 👉* https://multilaptops.net/producto/100279`)
+				productoFacebook(numero, "100354", "boy_text", "footer_text")
 			}
 			if (mensaje === 'Pro') {
 				productoFacebook(numero, "100279", `*Código SKU:* 100279
