81168ed6879bf935194b1aa5b767102f037dcf33
Antraxsec Flow x13 <antraxsec@gmail.com>
2023-08-17 16:58:02 -0400

    David 17/08/23 : 16:57--

--- src/app.js
+++ src/app.js
@@ -174,7 +174,7 @@ async function handleIncomingMessage(chatId, message) {
 			else {
 				client.sendMessage(numero, [
 					`Ingresa un número de celular válido.`,
-					` 1️⃣ Salir.`,
+					` 1️⃣ Salir.  `,
 				].join('\n'));
 				chatStates.set(chatId, "reenviarProcesoCompra");
 			}
